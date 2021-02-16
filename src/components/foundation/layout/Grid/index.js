fb54bc5b09de45ba5f3b4909b3b4ea4258c3f0c0
Igor Macedo <imacedo2000.im@gmail.com>
2021-02-15 23:32:53 -0300

    Aula 8

---
+++ src/components/foundation/layout/Grid/index.js
@@ -0,0 +1,89 @@
+import styled, { css } from "styled-components";
+import {breakpointsMedia} from "../../../../theme/utils/breakpointsMedia";
+
+export const Grid = {
+    Container: styled.div`
+      width: 100%;
+      padding-right: 28px;
+      padding-left: 28px;
+      margin-right: auto;
+      margin-left: auto;
+      max-width: initial;
+      ${breakpointsMedia({
+        sm: css`
+          max-width: 576px; 
+        `,
+        md: css`
+          max-width: 768px;
+          padding-right: 16px;
+          padding-left: 16px; 
+        `,
+        lg: css`
+          max-width: 1160px; 
+        `,
+        xl: css`
+          max-width: 1222px;
+        `,
+      })}
+    `,
+    Row: styled.div`
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -16px;
+      margin-left: -16px;
+    `,
+    Col: styled.div`
+      padding-right: 16px;
+      padding-left: 16px;
+      flex-basis: 0; //Força as colunas a ter a mesma largura sempre.
+      flex-grow: 1;
+      max-width: 100%;
+      
+      ${({value}) => {
+          if(typeof value === 'number') {
+              return css`
+                flex-grow: 0;
+                flex-shrink: 0;
+                flex-basis: ${(100 * value) / 12}%;
+                max-width: ${(100 * value) / 12}%;
+              `
+          }
+          
+          return breakpointsMedia({
+            xs: value?.xs
+            ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.xs) / 12}%;
+              max-width: ${(100 * value.xs) / 12}%;
+            ` : '',
+            md: value?.md
+            ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.md) / 12}%;
+              max-width: ${(100 * value.md) / 12}%;
+            ` : '',
+          });
+      }}
+
+      ${({ offset }) => {
+        if(typeof offset === 'number') {
+          return css`
+            margin-left: ${(100 * offset) / 12}%;
+          `
+        }
+        
+        return breakpointsMedia({
+            xs: offset?.xs
+            ? css`
+              margin-left: ${(100 * offset.xs) / 12}%};
+            ` : '',
+            md: offset?.md
+            ? css`
+              margin-left: ${(100 * offset.md) / 12}%};
+            ` : '',
+          });
+      }}
+    `,
+}
