0f1e137c8b468dd7b4c4dec6ad7d1c4d4a5edc3e
Igor Macedo <imacedo2000.im@gmail.com>
2021-02-28 17:55:48 -0300

    Consertando erros do Eslint

--- script-test.js
+++ script-test.js
@@ -1,5 +1,6 @@
 const shell = require('shelljs');
 
-shell.exect('git diff --name-only branch-scripts-testes..main');
+shell.exect('git diff --name-only branch-scripts-testes..main', { silent: true });
 
+// eslint-disable-next-line no-console
 console.log('Olá Mundo dos testes');
