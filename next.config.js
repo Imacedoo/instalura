840f2f8d3eec6930127943dc101bfb5c84f9d4b9
Igor Macedo <imacedo2000.im@gmail.com>
2021-03-12 19:47:17 -0300

    perf(nextconfig): add configs for headers

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-import redirects from './config/redirects';
+const redirects = require('./config/redirects');
+const headers = require('./config/headers');
 
 module.exports = {
   trailingSlash: true,
   async redirects() {
     return redirects;
+  },
+  async headers() {
+    return headers;
   },
 };
