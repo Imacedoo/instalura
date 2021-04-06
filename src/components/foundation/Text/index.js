f363be0a30aa5d954302a9631a9d0d3f67bc90b8
Igor Macedo <48502111+Imacedoo@users.noreply.github.com>
2021-04-05 21:07:38 -0300

    Update index.js

--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import get from 'lodash/get';
 import { propToStyle } from '../../../theme/utils/propToStyle';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 import Link from '../../commons/Link';
@@ -39,7 +40,7 @@ export const TextStyleVariantMap = {
 
 const TextBase = styled.span`
   ${(props) => TextStyleVariantMap[props.variant]};
-  color: ${(props) => _.get(props.theme, `colors.${props.color}.color`)};
+  color: ${(props) => get(props.theme, `colors.${props.color}.color`)};
 
   ${propToStyle('textAlign')};
   ${propToStyle('marginBottom')};
