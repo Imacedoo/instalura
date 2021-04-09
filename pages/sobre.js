ea556a12b1f1158b534c7df932a35f654e8491af
Igor Macedo <imacedo2000.im@gmail.com>
2021-04-08 22:20:54 -0300

    feat(page-sobre): creating our page getting informations from datto

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
+import AboutScreen from '../src/components/screens/AboutScreen';
 
-export default function PageSobre() {
-  return (
-    <div>
-      Página Sobre
-    </div>
-  );
+export async function getStaticProps() {
+  const TOKEN = '30c66d4b1f7f4dcfdc6fdb1a4df08d';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  const query = gql`
+    query {
+      pagesobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
---
+++ src/components/screens/AboutScreen/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '../../foundation/layout/Box';
+import Grid from '../../foundation/layout/Grid';
+import Text from '../../foundation/Text';
+
+export default function Index({ messages }) {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      flex={1}
+    >
+      <Grid.Container>
+        <Grid.Row
+          marginTop={{ xs: '32px', md: '120px' }}
+          flex="1"
+        >
+          <Grid.Col
+            value={{ xs: 12, md: 6, lg: 6 }}
+            offset={{ md: 2 }}
+            flex={1}
+          >
+            <Text
+              variant="title"
+              tag="h2"
+              color="tertiary.main"
+            >
+              {messages.pagesobre.pageTitle}
+            </Text>
+
+            <Box
+              dangerouslySetInnerHTML={{
+                __html: messages.pagesobre.pageDescription,
+              }}
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+    </Box>
+  );
+}
+
+Index.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  messages: PropTypes.object.isRequired,
+};
