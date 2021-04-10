b6130663f29d779dbee531c4fc0e57e7abaacc05
Igor Macedo <imacedo2000.im@gmail.com>
2021-04-09 21:59:16 -0300

    feat(cms_client): add new function to abstract the implementation of our CMS

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,25 +1,8 @@
-import { GraphQLClient, gql } from 'graphql-request';
 import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
-import AboutScreen from '../src/components/screens/AboutScreen';
+import AboutScreen, { getContent } from '../src/components/screens/AboutScreen';
 
 export async function getStaticProps() {
-  const TOKEN = process.env.DATO_CMS_TOKEN;
-  const DatoCMSURL = 'https://graphql.datocms.com/';
-  const client = new GraphQLClient(DatoCMSURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-  const query = gql`
-    query {
-      pagesobre {
-        pageTitle
-        pageDescription
-      }
-    }
-  `;
-
-  const messages = await client.request(query);
+  const messages = await getContent();
 
   return {
     props: {
