8dd0cd8520f671f36a9b7f9fa61254e587e9c5e1
Igor Macedo <imacedo2000.im@gmail.com>
2021-04-15 00:16:26 -0300

    fix(login_service_tests): fix test on cypress in go to profile page step

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -18,7 +18,7 @@ describe('/pages/app/login/', () => {
         .then((intercept) => {
           const { token } = intercept.response.body.data;
 
-          cy.getCookie('APP_TOKEN')
+          cy.getCookie('LOGIN_COOKIE_APP_TOKEN')
             .should('exist')
             .should('have.property', 'value', token);
         });
