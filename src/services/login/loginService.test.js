f643c5e8d7dc0e11e23a1e5ee5c5646b3cd50fe4
Igor Macedo <imacedo2000.im@gmail.com>
2021-04-15 00:05:30 -0300

    fix(login_service_tests): fix test on expect from logout function

--- src/services/login/loginService.test.js
+++ src/services/login/loginService.test.js
@@ -60,7 +60,7 @@ describe('loginService', () => {
         const destroyCookie = jest.fn();
         await loginService.logout(null, destroyCookie);
 
-        expect(destroyCookie).toHaveBeenCalledWith(null, 'APP_TOKEN');
+        expect(destroyCookie).toHaveBeenCalledWith(null, 'LOGIN_COOKIE_APP_TOKEN');
       });
     });
   });
